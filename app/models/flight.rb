d5584747888782c7e4f6fdc2b8d415ee57282a05
soadaa <Soadhlh@gmail.com>
2024-01-27 19:05:53 -0500

    Third pass at model associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,7 @@
 class Flight < ApplicationRecord
+  belongs_to :departure_airport, class_name: 'Airport', foreign_key: 'departure_airport_id'
+  belongs_to :arrival_airport, class_name: 'Airport', foreign_key: 'arrival_airport_id'
+
   has_many :flight_routes
   has_many :airports, through: :flight_routes
 end
