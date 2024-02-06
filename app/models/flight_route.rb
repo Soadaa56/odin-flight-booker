dc64c96965be5ef8fe6b442645b28563971e2274
soadaa <Soadhlh@gmail.com>
2024-02-06 18:57:44 -0500

    Update flightroutes model for date time

--- app/models/flight_route.rb
+++ app/models/flight_route.rb
@@ -2,4 +2,6 @@ class FlightRoute < ApplicationRecord
   belongs_to :flight
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
+  attribute :departure_date, :date
+  attribute :departure_time, :time
 end
